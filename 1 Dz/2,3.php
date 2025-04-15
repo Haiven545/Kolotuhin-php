18dc10f61725607490dfd095ebf189c6de73a2b6
DmitriiKolotuhin <haivenspiret@gmail.com>
2025-04-15 12:29:58 +0300

    add 1 Dz

--- 1 Dz/2,3.php
+++ 1 Dz/2,3.php
@@ -1,12 +1,14 @@
 <?php
+
 $lastName = "иванов";
 $firstName = "иван";
 $middleName = "иванович";
 
-$formattedLastName = ucfirst($lastName);
 
-$firstInitial = strtoupper(substr($firstName, 0, 1));
-$middleInitial = strtoupper(substr($middleName, 0, 1));
+$formattedLastName = mb_convert_case($lastName, MB_CASE_TITLE, "UTF-8");
+
+$firstInitial = mb_strtoupper(mb_substr($firstName, 0, 1, "UTF-8"), "UTF-8");
+$middleInitial = mb_strtoupper(mb_substr($middleName, 0, 1, "UTF-8"), "UTF-8");
 
 echo $formattedLastName . " " . $firstInitial . "." . $middleInitial . ".";
 ?>
