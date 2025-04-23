11594b7c372f1e572a10c1fbbd1f0be3c6d2ae2e
Haiven545 <151510644+Haiven545@users.noreply.github.com>
2025-04-23 12:37:08 +0300

    Update 0001_01_01_000000_create_users_table.php

--- Laravel/my-project/database/migrations/0001_01_01_000000_create_users_table.php
+++ Laravel/my-project/database/migrations/0001_01_01_000000_create_users_table.php
@@ -6,15 +6,13 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique()->index(); // Добавлен индекс
+            $table->string('email')->unique()->index();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
@@ -25,7 +23,7 @@ return new class extends Migration
             $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
-            $table->timestamp('expires_at')->nullable(); // Добавлено время жизни токена
+            $table->timestamp('expires_at')->nullable();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
@@ -34,7 +32,7 @@ return new class extends Migration
                 ->nullable()
                 ->index()
                 ->constrained()
-                ->cascadeOnDelete(); // Добавлен каскадный удаление
+                ->cascadeOnDelete();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
@@ -42,9 +40,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
